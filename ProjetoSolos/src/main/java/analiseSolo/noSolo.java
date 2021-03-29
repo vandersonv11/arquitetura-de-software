0cf98a549f098d8b594eb9e2fe6d4a23c375e30a
vandersonv11 <vandersonv11@hotmail.com>
2021-03-29 17:48:34 -0300

    criando classe nosolo

---
+++ ProjetoSolos/src/main/java/analiseSolo/noSolo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analiseSolo;
+
+import java.util.ArrayList;
+import solo.Elementos;
+
+/**
+ *
+ * @author vande
+ */
+public class noSolo {
+    private ArrayList <Elementos> elementosNoSolo = new ArrayList<Elementos>();
+
+    /**
+     * @return the elementosNoSolo
+     */
+    public ArrayList <Elementos> getElementosNoSolo() {
+        return elementosNoSolo;
+    }
+
+    /**
+     * @param elementosNoSolo the elementosNoSolo to set
+     */
+    public void setElementosNoSolo(ArrayList <Elementos> elementosNoSolo) {
+        this.elementosNoSolo = elementosNoSolo;
+    }
+
+    
+}
