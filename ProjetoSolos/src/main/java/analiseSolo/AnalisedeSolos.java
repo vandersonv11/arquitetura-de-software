33eef632168761a134f04d4b715146cd44be81b6
vandersonv11 <vandersonv11@hotmail.com>
2021-03-31 18:15:02 -0300

    correção de codigo

--- ProjetoSolos/src/main/java/analiseSolo/AnalisedeSolos.java
+++ ProjetoSolos/src/main/java/analiseSolo/AnalisedeSolos.java
@@ -6,6 +6,7 @@
 package analiseSolo;
 
 import java.util.ArrayList;
+import solo.Amostra;
 import solo.Elementos;
 
 /**
@@ -13,5 +14,43 @@ import solo.Elementos;
  * @author vande
  */
 public class AnalisedeSolos {
+   private noSolo analiseNoSolo;
+   private Ideal analiseIdeal;
+   private Amostra analiseAmostra;
+
+    public AnalisedeSolos() {
+        analiseNoSolo = new noSolo();
+        analiseIdeal = new Ideal();
+        analiseAmostra = new Amostra();
+    }
+
+    /**
+     * @return the analiseNoSolo
+     */
+    public noSolo getAnaliseNoSolo() {
+        return analiseNoSolo;
+    }
+
+    /**
+     * @param analiseNoSolo the analiseNoSolo to set
+     */
+    public void setAnaliseNoSolo(noSolo analiseNoSolo) {
+        this.analiseNoSolo = analiseNoSolo;
+    }
+
+    /**
+     * @return the analiseIdeal
+     */
+    public Ideal getAnaliseIdeal() {
+        return analiseIdeal;
+    }
+
+    /**
+     * @param analiseIdeal the analiseIdeal to set
+     */
+    public void setAnaliseIdeal(Ideal analiseIdeal) {
+        this.analiseIdeal = analiseIdeal;
+    }
+   
    
 }
